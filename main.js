cd59aa5dfb552c1e3b9810d5f731d512803a30ec
Srdjan S <stankovic.srdjo@gmail.com>
2025-02-07 15:16:52 +0100

    Add usage for main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//
+// Usage:
+// zinnia run main.js
+//
+// roundNum = -1 for current round by default; adjust to your desired round number
+// maxTasks = -1 for all tasks by default; adjust to the desired number of tasks you want to run
+// dagScope = all, block, entity; by default set to entity (retrieves byte range)
+// minerId = undefined by default; set to miner id if you want to spot check specific miner (storage provider)
+//
+
 import SpotChecker from './lib/spot-checker.js'
 
 const roundNum = -1 // current
