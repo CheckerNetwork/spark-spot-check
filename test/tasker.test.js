00c2f4542c3981afa7bac4b17ab1f90421608a6a
Srdjan S <stankovic.srdjo@gmail.com>
2025-02-07 14:24:54 +0100

    Change how tasker tasks are selected

--- test/tasker.test.js
+++ test/tasker.test.js
@@ -2,10 +2,64 @@
 
 import { test } from 'zinnia:test'
 import { assertEquals } from 'zinnia:assert'
-import { pickRandomTasks } from '../lib/tasker.js'
+import { pickTasks } from '../lib/tasker.js'
 
-test('pickRandomTasks', async () => {
-  const allTasks = [
+test('pickTasks - no miner or limit specified', async () => {
+  const tasks = givenTasks()
+  const selectedTasks = await pickTasks({
+    tasks,
+    maxTasks: -1,
+  })
+
+  assertEquals(selectedTasks, tasks)
+})
+
+
+test('pickTasks - miner f010', async () => {
+  const tasks = givenTasks()
+  const selectedTasks = await pickTasks({
+    tasks,
+    minerId: 'f010',
+    maxTasks: -1,
+  })
+
+  assertEquals(selectedTasks, [
+    { cid: 'bafyone', minerId: 'f010' },
+    { cid: 'bafytwo', minerId: 'f010' }
+  ])
+})
+
+
+test('pickTasks - miner f010 and limit 1', async () => {
+  const tasks = givenTasks()
+  const selectedTasks = await pickTasks({
+    tasks,
+    minerId: 'f010',
+    maxTasks: 1,
+  })
+
+  assertEquals(selectedTasks, [
+    { cid: 'bafyone', minerId: 'f010' },
+  ])
+})
+
+test('pickTasks - limit 3', async () => {
+  const tasks = givenTasks()
+  const selectedTasks = await pickTasks({
+    tasks,
+    maxTasks: 3,
+  })
+
+  assertEquals(selectedTasks, [
+    { cid: 'bafyone', minerId: 'f010' },
+    { cid: 'bafyone', minerId: 'f020' },
+    { cid: 'bafyone', minerId: 'f030' }
+  ])
+})
+
+
+function givenTasks() {
+  return [
     { cid: 'bafyone', minerId: 'f010' },
     { cid: 'bafyone', minerId: 'f020' },
     { cid: 'bafyone', minerId: 'f030' },
@@ -16,11 +70,5 @@ test('pickRandomTasks', async () => {
     { cid: 'bafytwo', minerId: 'f030' },
     { cid: 'bafytwo', minerId: 'f040' }
   ]
+}
 
-  const selectedTasks = await pickRandomTasks({
-    tasks: allTasks,
-    numTasks: 3
-  })
-
-  assertEquals(selectedTasks.length, 3)
-})
