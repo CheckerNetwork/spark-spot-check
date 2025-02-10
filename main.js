5584c3a79b22c62af0e686eca53364ec1c3503f9
Srdjan S <stankovic.srdjo@gmail.com>
2025-02-10 17:25:38 +0100

    Apply feedback from Julian

--- main.js
+++ main.js
@@ -10,11 +10,11 @@
 
 import SpotChecker from './lib/spot-checker.js'
 
-const roundNum = -1 // current
-const maxTasks = -1 // all tasks
+const roundId = -1 // current
+const maxTasks = 50 // all tasks
 const dagScope = 'entity'
 const entityBytesRange = '0:200' // fetch first 200 mb of the file
 const minerId = undefined // by default do not filter for specific miner tasks
 
 const checker = new SpotChecker()
-await checker.run({ roundNum, maxTasks, minerId, dagScope, entityBytesRange })
+await checker.run({ roundId, maxTasks, minerId, dagScope, entityBytesRange })
