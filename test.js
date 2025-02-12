c7238442cfc466033fc1e35d8ba1cb78cf1b5adc
Srdjan S <stankovic.srdjo@gmail.com>
2025-02-12 16:06:56 +0100

    Reogranise imports in test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import './test/ipni-client.test.js'
 import './test/miner-info.test.js'
-import './test/integration.js'
 import './test/multiaddr.test.js'
+
+import './test/integration.js'
 import './test/spot-checker.js'
